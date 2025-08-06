352ee45c1432155a3399aa22f601756dffd19a5e
Hasan <robiulhasanjisan88@gmail.com>
2025-08-06 22:04:06 +0600

    data

--- src/HomeExtra/ServicesSection.jsx
+++ src/HomeExtra/ServicesSection.jsx
@@ -1,18 +1,11 @@
+// ServicesSection.jsx or .js
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { Helmet, HelmetProvider } from "react-helmet-async";
+import services from "../data/services"; 
 
-const services = [
-  { name: "Draft Survey", image: "/image/draft survey.jpg" },
-  { name: "On / Off Hire Survey", image: "/image/bunker.jpg" },
-  { name: "Cargo Hold Cleanliness Inspection", image: "/image/Cargo-hold-cleaning-inspection.jpg" },
-  { name: "Cargo Holds Water-tightness Test / Sealing", image: "/image/water tightness.jpg" },
-  { name: "Preparatory Port State Control Inspection", image: "/image/port control inspections.jpg" },
-  { name: "Container / Cargo Damage Surveys", image: "/image/cargo damage.jpg" },
-  { name: "Container / Flatrack Lashing Surveys", image: "/image/flatrack LASHING-SURVEY.jpg" },
-  { name: "General Superintendence", image: "/image/General Superintendence.jpg" },
-  { name: "P and I Clubs", image: "/image/P and I Clubs.webp" },
-];
+
+
 
 export default function ServicesSection() {
   const navigate = useNavigate();
---
+++ src/data/services.js
@@ -0,0 +1,14 @@
+// src/data/services.js
+const services = [
+  { name: "Draft Survey", image: "/image/draft survey.jpg" },
+  { name: "On / Off Hire Survey", image: "/image/bunker.jpg" },
+  { name: "Cargo Hold Cleanliness Inspection", image: "/image/Cargo-hold-cleaning-inspection.jpg" },
+  { name: "Cargo Holds Water-tightness Test / Sealing", image: "/image/water tightness.jpg" },
+  { name: "Preparatory Port State Control Inspection", image: "/image/port control inspections.jpg" },
+  { name: "Container / Cargo Damage Surveys", image: "/image/cargo damage.jpg" },
+  { name: "Container / Flatrack Lashing Surveys", image: "/image/flatrack LASHING-SURVEY.jpg" },
+  { name: "General Superintendence", image: "/image/General Superintendence.jpg" },
+  { name: "P and I Clubs", image: "/image/P and I Clubs.webp" },
+];
+
+export default services;
